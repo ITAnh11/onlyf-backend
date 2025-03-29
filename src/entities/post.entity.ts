cf04aff12dbfaa3e010e4799e22f835811371c8f
Tuan Anh <buianhkc112004@gmail.com>
2025-03-29 19:12:23 +0700

    feat (post): thêm chức năng tạo post, get my post

---
+++ src/entities/post.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: true,
+  })
+  caption: string;
+
+  @Column({
+    nullable: true,
+  })
+  urlPublicImage: string;
+
+  @Column({
+    nullable: true,
+  })
+  pathImage: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.refreshTokens)
+  @JoinColumn()
+  user: User;
+}

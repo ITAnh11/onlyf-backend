4980a59a7bf549967ac1ed32a8a28f197e24f97e
ITAnh11 <buianhkc112004@gmail.com>
2025-04-19 18:28:55 +0700

    feat(notification): xoa fcm token khi logout

--- src/modules/refresh_token/refresh_token.module.ts
+++ src/modules/refresh_token/refresh_token.module.ts
@@ -6,12 +6,14 @@ import { ConfigModule } from '@nestjs/config';
 import { RefreshToken } from 'src/entities/refresh-token.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { RefreshTokenCronService } from './refresh-token-cron.service';
+import { FcmTokenModule } from '../fcm_token/fcm_token.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([RefreshToken]),
     JwtModule,
     ConfigModule.forRoot(),
+    FcmTokenModule,
   ],
   controllers: [RefreshTokenController],
   providers: [RefreshTokenService, RefreshTokenCronService],
